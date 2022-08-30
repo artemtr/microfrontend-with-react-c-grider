af9ae49ff9c52223767c56401dce6b715f4caa25
Artem Travlo <info.travlo@gmail.com>
2022-08-30 16:15:37 +0300

    Testing deploy to aws

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      exposes: {
+        './Marketing': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
