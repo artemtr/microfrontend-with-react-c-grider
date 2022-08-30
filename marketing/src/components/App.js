7981a2cf0a2a8574a3064acc33d4a785e52d1e57
Artem Travlo <info.travlo@gmail.com>
2022-08-30 13:56:53 +0300

    move to 5 dir.

---
+++ marketing/src/components/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Landing from './Landing';
+import Pricing from './Pricing';
+function App() {
+  return (
+    <div>
+      <Landing />
+      <Pricing />
+    </div>
+  );
+}
+
+export default App;
