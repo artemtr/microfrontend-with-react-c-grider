db1f9b908e48178b18fc893b45653171b7efa240
Artem Travlo <info.travlo@gmail.com>
2022-08-30 14:54:37 +0300

    Move to folder 6

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -6,17 +6,15 @@ const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   devServer: {
-    port: 8081,
+    port: 8080,
     historyApiFallback: {
       index: 'index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'marketing',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './Marketing': './src/bootstrap',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
       },
       shared: packageJson.dependencies,
     }),
---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { mount } from 'marketing/Marketing';
+import MarketingApp from './components/MarketingApp';
+export default () => {
+  return (
+    <div>
+      Home page
+      <MarketingApp />
+    </div>
+  );
+};
